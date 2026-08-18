8f8555ea6bb6ff1ac25c0326d06c8e993f7e024b
Wesley Coutinho <105899404+WesleyCouti@users.noreply.github.com>
2026-08-17 21:38:58 -0300

    fix: correct cart page validation

--- cypress/pages/CartPage.ts
+++ cypress/pages/CartPage.ts
@@ -1,18 +1,17 @@
 export class CartPage {
-  private readonly cartItem = '[data-test="cart-item"]';
   private readonly productName = '[data-test="inventory-item-name"]';
   private readonly checkoutButton = '[data-test="checkout"]';
-
-  assertProduct(productName: string): void {
-    cy.contains(this.productName, productName)
-      .should('be.visible');
-  }
 
   assertCartLoaded(): void {
     cy.url().should('include', '/cart.html');
 
-    cy.get(this.cartItem)
-      .should('have.length.greaterThan', 0);
+    cy.get(this.checkoutButton)
+      .should('be.visible');
+  }
+
+  assertProduct(productName: string): void {
+    cy.contains(this.productName, productName)
+      .should('be.visible');
   }
 
   startCheckout(): void {
