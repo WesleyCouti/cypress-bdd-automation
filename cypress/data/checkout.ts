a18d19415f847c2375340880e36895331734ec58
Wesley Coutinho <105899404+WesleyCouti@users.noreply.github.com>
2026-08-17 21:33:30 -0300

    Add type satisfaction for checkoutCustomer

--- cypress/data/checkout.ts
+++ cypress/data/checkout.ts
@@ -1,5 +1,7 @@
+import type { CheckoutData } from '../pages/CheckoutPage';
+
 export const checkoutCustomer = {
   firstName: 'Wesley',
   lastName: 'Coutinho',
   postalCode: '06000-000'
-} as const;
+} satisfies CheckoutData;
