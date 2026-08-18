398bff3b9f6628cfc31f1040863d91fc65085c87
Wesley Coutinho <105899404+WesleyCouti@users.noreply.github.com>
2026-08-17 21:18:55 -0300

    refactor: improve checkout page flow validation

--- cypress/pages/CheckoutPage.ts
+++ cypress/pages/CheckoutPage.ts
@@ -5,19 +5,48 @@ export type CheckoutData = {
 };
 
 export class CheckoutPage {
+  private readonly firstNameInput = '[data-test="firstName"]';
+  private readonly lastNameInput = '[data-test="lastName"]';
+  private readonly postalCodeInput = '[data-test="postalCode"]';
+  private readonly continueButton = '[data-test="continue"]';
+  private readonly finishButton = '[data-test="finish"]';
+  private readonly completeHeader = '[data-test="complete-header"]';
+
   fillCustomerData(data: CheckoutData): void {
-    cy.get('[data-test="firstName"]').type(data.firstName);
-    cy.get('[data-test="lastName"]').type(data.lastName);
-    cy.get('[data-test="postalCode"]').type(data.postalCode);
-    cy.get('[data-test="continue"]').click();
+    cy.get(this.firstNameInput)
+      .should('be.visible')
+      .clear()
+      .type(data.firstName);
+
+    cy.get(this.lastNameInput)
+      .should('be.visible')
+      .clear()
+      .type(data.lastName);
+
+    cy.get(this.postalCodeInput)
+      .should('be.visible')
+      .clear()
+      .type(data.postalCode);
+
+    cy.get(this.continueButton)
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+
+    cy.url().should('include', '/checkout-step-two.html');
   }
 
   finishOrder(): void {
-    cy.get('[data-test="finish"]').click();
+    cy.get(this.finishButton)
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+
+    cy.url().should('include', '/checkout-complete.html');
   }
 
   assertCompleted(): void {
-    cy.get('[data-test="complete-header"]')
+    cy.get(this.completeHeader)
       .should('be.visible')
       .and('have.text', 'Thank you for your order!');
   }
