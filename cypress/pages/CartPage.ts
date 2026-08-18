3c869e40c7bb5433ca139b9b573654820c25ebbd
Wesley Coutinho <105899404+WesleyCouti@users.noreply.github.com>
2026-08-17 21:17:29 -0300

    refactor: improve cart page validation and navigation

--- cypress/pages/CartPage.ts
+++ cypress/pages/CartPage.ts
@@ -1,9 +1,26 @@
 export class CartPage {
+  private readonly cartItem = '[data-test="cart-item"]';
+  private readonly productName = '[data-test="inventory-item-name"]';
+  private readonly checkoutButton = '[data-test="checkout"]';
+
   assertProduct(productName: string): void {
-    cy.contains('[data-test="inventory-item-name"]', productName).should('be.visible');
+    cy.contains(this.productName, productName)
+      .should('be.visible');
+  }
+
+  assertCartLoaded(): void {
+    cy.url().should('include', '/cart.html');
+
+    cy.get(this.cartItem)
+      .should('have.length.greaterThan', 0);
   }
 
   startCheckout(): void {
-    cy.get('[data-test="checkout"]').click();
+    cy.get(this.checkoutButton)
+      .should('be.visible')
+      .and('be.enabled')
+      .click();
+
+    cy.url().should('include', '/checkout-step-one.html');
   }
 }
